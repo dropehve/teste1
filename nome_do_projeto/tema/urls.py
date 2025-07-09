796a57ae684bab96ef29f1fb139530299c8c00d1
dropehve <pedroh_esteves@hotmail.com>
2025-07-09 19:31:49 -0300

    Atualizar o urls.py

--- nome_do_projeto/tema/urls.py
+++ nome_do_projeto/tema/urls.py
@@ -4,3 +4,8 @@ from . import views
 urlpatterns = [
     path('listar-lesoes/', views.listar_lesoes, name='listar_lesoes'),
 ]
+
+urlpatterns += [
+    path('buscar/', views.buscar, name='buscar'),
+    path('resultado/', views.resultado_busca, name='resultado'),
+]
