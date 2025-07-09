9aafff1f0f5da17af18deb6c1809690a77ad328b
dropehve <pedroh_esteves@hotmail.com>
2025-07-09 19:23:12 -0300

    Atualizar o views.py

--- nome_do_projeto/tema/views.py
+++ nome_do_projeto/tema/views.py
@@ -15,3 +15,11 @@ from .models import Lesao  # substitua pelo nome real do seu modelo
 def listar_dados(request):
     dados = Lesao.objects.all()
     return render(request, 'tema/lista.html', {'dados': dados})
+    
+def buscar(request):
+    return render(request, 'tema/busca.html')
+
+def resultado_busca(request):
+    termo = request.GET.get('termo')
+    resultados = SeuModelo.objects.filter(nome__icontains=termo)
+    return render(request, 'tema/resultado.html', {'resultados': resultados})
